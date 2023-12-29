0bc11021314b0c8a61034233ad648b8d129f9817
Aniruddh8877 <sharmaaniruddh811@gmail.com>
2023-12-29 14:49:34 +0530

    adding traversal program

--- traverse-array.c
+++ traverse-array.c
@@ -1,14 +1,17 @@
-#include<stdio.h>
-void traverse(int*,int);
-void main(){
-int arr[5]={2,3,4,5,6};
-int N=5;
-printf("press any key to perform array traversal and display its elements:\n\n");
-traverse(arr,N);
+#include <stdio.h>
+void traverse(int *, int);
+void main()
+{
+    int arr[5] = {2, 3, 4, 5, 6};
+    int N = 5;
+    printf("press any key to perform array traversal and display its elements:\n\n");
+    traverse(arr, N);
 }
-void traverse(int *array,int size){
+void traverse(int *array, int size)
+{
     int i;
-    for(i=0;i<size; i++){
-        printf("arr[%d] = %d\n",i,array[i]);
+    for (i = 0; i < size; i++)
+    {
+        printf("arr[%d] = %d\n", i, array[i]);
     }
 }
